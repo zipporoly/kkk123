430b5e4b29ffde8f6d26fffde018df7f6f20abab
NeverMoreShadowFiend <1749927047@qq.com>
2022-05-30 15:14:33 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "fd1d2ccc-6fd3-4e49-b3b9-9b85516633ed",
                         "alterId": 0
                     }
                 ],
